a94f2fd05dbe59c50f8dccae73d9be91ca74e7df
Faizan <syedf928@gmail.com>
2025-04-30 11:34:15 +0530

    make side  proper for mobile  view

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,27 @@
+// import { Outlet } from "react-router-dom";
+// import Sidebar from "./Sidebar";
 
+// const Layout = () => {
+//   return (
+//     <div className="flex min-h-screen">
+//       <Sidebar />
+//       <div className="ml-64 p-6 w-full bg-gray-100">
+//         <Outlet />
+//       </div>
+//     </div>
+//   );
+// };
 
-import { Outlet } from "react-router-dom";
+// export default Layout;
+
 import Sidebar from "./Sidebar";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex">
       <Sidebar />
-      <div className="ml-64 p-6 w-full bg-gray-100">
+      <div className="flex-1 md:ml-64 p-4">
         <Outlet />
       </div>
     </div>
